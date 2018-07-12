c6108dcbb81205039801e334facaea673dab46b4
ameliebarrero@icloud.com <ameliebarrero@icloud.com>
2018-07-12 21:27:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function printBadges(employees){
   for (var count=0;count<employees.length;count++){
-    console.log("Welcome " +employees[count]+"! You are employee #"+(count+1)+".")
+    console.log("Welcome " +employees[count]+"! You are employee #"+(count+1)+".");
     return employees
   }
 }
